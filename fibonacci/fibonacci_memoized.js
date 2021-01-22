bd25eeb31ff80c0dbde1d4e09ae57f48e0ee5dbd
Bartosz <bartosz@wisniewski.pl>
2021-01-22 20:19:17 +0100

    fibonacci recusive and memoized

---
+++ fibonacci/fibonacci_memoized.js
@@ -0,0 +1,11 @@
+function fibonacci(n, memo = {}) {
+    if (n in memo) return memo[n]
+    if (n === 1 || n === 2) return 1;
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+    return memo[n];
+}
+
+console.log(fibonacci(4));
+console.log(fibonacci(5));
+console.log(fibonacci(6));
+console.log(fibonacci(100));
